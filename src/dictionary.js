fad5bf76cbbfeb6c58efa9cbe1cfb822cc824bfc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-22 10:17:04 -0500

    feat(dictionary): a few more words

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,11 +1,17 @@
-const modifiers = ['try to', 'avoid', 'skip', '', '', '', '', '']
+// add empty modifiers to sometimes not include one
+const modifiers = ['try to', 'avoid', 'skip', 'pretend to',
+  'help', '', '', '', '', '']
+
 const verbs = [
   'learn', 'clean', 'buy', 'pick', 'do', 'make', 'fix', 'exercise',
-  'tweet', 'promote', 'code', 'play', 'find'
+  'tweet', 'promote', 'code', 'play', 'find', 'crash', 'submit',
+  'skip', 'add', 'forget', 'avoid', 'throw', 'buy', 'sell'
 ]
+
 const nouns = [
-  'Italian', 'milk', 'needle work', 'chess', 'Node.js',
-  'books', 'boots', 'fishing rod', 'distant relatives'
+  'Italian', 'milk', 'needle work', 'chess', 'Node.js', 'fines',
+  'books', 'boots', 'fishing rod', 'distant relatives', 'charges',
+  'knife', 'castle', 'laptop', 'principles', 'adults', 'bird'
 ]
 
 module.exports = {
